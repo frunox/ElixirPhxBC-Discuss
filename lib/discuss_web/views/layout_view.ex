511a8dd5fe76968fc1633c5a1c9ff9c57d16fa63
frunox <jrcannon@msn.com>
2022-02-06 17:01:50 -0600

    fix warnings in view files - add template_not_found()

--- lib/discuss_web/views/layout_view.ex
+++ lib/discuss_web/views/layout_view.ex
@@ -1,6 +1,13 @@
 defmodule DiscussWeb.LayoutView do
   use DiscussWeb, :view
 
+    # By default, Phoenix returns the status message from
+  # the template name. For example, "404.html" becomes
+  # "Not Found".
+  def template_not_found(template, _assigns) do
+    Phoenix.Controller.status_message_from_template(template)
+  end
+
   # Phoenix LiveDashboard is available only in development by default,
   # so we instruct Elixir to not warn if the dashboard route is missing.
   @compile {:no_warn_undefined, {Routes, :live_dashboard_path, 2}}
