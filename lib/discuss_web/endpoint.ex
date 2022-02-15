84b8bfc479fc2dd9c8ca1773868d6f5eb99289de
frunox <jrcannon@msn.com>
2022-02-15 16:07:57 -0600

    successful implementation of comments channel

--- lib/discuss_web/endpoint.ex
+++ lib/discuss_web/endpoint.ex
@@ -10,6 +10,10 @@ defmodule DiscussWeb.Endpoint do
     signing_salt: "M6reO+vT"
   ]
 
+  socket "/socket", DiscussWeb.UserSocket,
+    websocket: true,
+    longpoll: false
+
   socket "/live", Phoenix.LiveView.Socket, websocket: [connect_info: [session: @session_options]]
 
   # Serve at "/" the static files from "priv/static" directory.
